4fb207406304e2d32d312cc07708b2e7c1aaa625
austinhoag <austinthomashoag@gmail.com>
2020-06-28 19:38:02 -0400

    remove forms.py

--- condis/main/routes.py
+++ condis/main/routes.py
@@ -1,19 +1,9 @@
 from flask import Blueprint, render_template, request
-from .forms import SetupForm
 import requests
 import pandas
 from functools import reduce
 from datetime import datetime,timedelta
 import pandas as pd
-
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
-
-from bokeh.io import curdoc
-from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, Slider, TextInput
 
 import numpy as np
 
@@ -138,100 +128,100 @@ def setup():
 			all_print_strs=all_print_strs,no_results=False)
 	return render_template('main/setup_form.html')
 
-@main.route('/bokeh')
-def bokeh():
+# @main.route('/bokeh')
+# def bokeh():
 
-    # init a basic bar chart:
-    # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
-    fig = figure(plot_width=600, plot_height=600)
-    fig.vbar(
-        x=[1, 2, 3, 4],
-        width=0.5,
-        bottom=0,
-        top=[1.7, 2.2, 4.6, 3.9],
-        color='navy'
-    )
+#     # init a basic bar chart:
+#     # http://bokeh.pydata.org/en/latest/docs/user_guide/plotting.html#bars
+#     fig = figure(plot_width=600, plot_height=600)
+#     fig.vbar(
+#         x=[1, 2, 3, 4],
+#         width=0.5,
+#         bottom=0,
+#         top=[1.7, 2.2, 4.6, 3.9],
+#         color='navy'
+#     )
 
-    # grab the static resources
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
+#     # grab the static resources
+#     js_resources = INLINE.render_js()
+#     css_resources = INLINE.render_css()
 
-    # render template
-    script, div = components(fig)
-    html = render_template(
-        'index.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
-    return encode_utf8(html)
+#     # render template
+#     script, div = components(fig)
+#     html = render_template(
+#         'index.html',
+#         plot_script=script,
+#         plot_div=div,
+#         js_resources=js_resources,
+#         css_resources=css_resources,
+#     )
+#     return encode_utf8(html)
 
-@main.route('/bokeh2')
-def bokeh2():
+# @main.route('/bokeh2')
+# def bokeh2():
 
-    # Set up data
-	N = 200
-	x = np.linspace(0, 4*np.pi, N)
-	y = np.sin(x)
-	source = ColumnDataSource(data=dict(x=x, y=y))
-
-
-	# Set up plot
-	plot = figure(plot_height=400, plot_width=400, title="my sine wave",
-	              tools="crosshair,pan,reset,save,wheel_zoom",
-	              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
-
-	plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
+#     # Set up data
+# 	N = 200
+# 	x = np.linspace(0, 4*np.pi, N)
+# 	y = np.sin(x)
+# 	source = ColumnDataSource(data=dict(x=x, y=y))
 
 
-	# Set up widgets
-	text = TextInput(title="title", value='my sine wave')
-	offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
-	amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
-	phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
-	freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1, step=0.1)
+# 	# Set up plot
+# 	plot = figure(plot_height=400, plot_width=400, title="my sine wave",
+# 	              tools="crosshair,pan,reset,save,wheel_zoom",
+# 	              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
+
+# 	plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
 
-	# Set up callbacks
-	def update_title(attrname, old, new):
-	    plot.title.text = text.value
-
-	text.on_change('value', update_title)
-
-	def update_data(attrname, old, new):
-
-	    # Get the current slider values
-	    a = amplitude.value
-	    b = offset.value
-	    w = phase.value
-	    k = freq.value
-
-	    # Generate the new curve
-	    x = np.linspace(0, 4*np.pi, N)
-	    y = a*np.sin(k*x + w) + b
-
-	    source.data = dict(x=x, y=y)
-
-	for w in [offset, amplitude, phase, freq]:
-	    w.on_change('value', update_data)
+# 	# Set up widgets
+# 	text = TextInput(title="title", value='my sine wave')
+# 	offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
+# 	amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
+# 	phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
+# 	freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1, step=0.1)
 
 
-	# Set up layouts and add to document
-	inputs = column(text, offset, amplitude, phase, freq)
+# 	# Set up callbacks
+# 	def update_title(attrname, old, new):
+# 	    plot.title.text = text.value
 
-	curdoc().add_root(row(inputs, plot, width=800))
-	curdoc().title = "Sliders"
-	# render template
-	script, div = components(plot)
-	# grab the static resources
-	js_resources = INLINE.render_js()
-	css_resources = INLINE.render_css()
-	html = render_template(
-	    'index.html',
-	    plot_script=script,
-	    plot_div=div,
-	    js_resources=js_resources,
-	    css_resources=css_resources,
-	)
-	return encode_utf8(html)
+# 	text.on_change('value', update_title)
+
+# 	def update_data(attrname, old, new):
+
+# 	    # Get the current slider values
+# 	    a = amplitude.value
+# 	    b = offset.value
+# 	    w = phase.value
+# 	    k = freq.value
+
+# 	    # Generate the new curve
+# 	    x = np.linspace(0, 4*np.pi, N)
+# 	    y = a*np.sin(k*x + w) + b
+
+# 	    source.data = dict(x=x, y=y)
+
+# 	for w in [offset, amplitude, phase, freq]:
+# 	    w.on_change('value', update_data)
+
+
+# 	# Set up layouts and add to document
+# 	inputs = column(text, offset, amplitude, phase, freq)
+
+# 	curdoc().add_root(row(inputs, plot, width=800))
+# 	curdoc().title = "Sliders"
+# 	# render template
+# 	script, div = components(plot)
+# 	# grab the static resources
+# 	js_resources = INLINE.render_js()
+# 	css_resources = INLINE.render_css()
+# 	html = render_template(
+# 	    'index.html',
+# 	    plot_script=script,
+# 	    plot_div=div,
+# 	    js_resources=js_resources,
+# 	    css_resources=css_resources,
+# 	)
+# 	return encode_utf8(html)
